a52e58b504beb6009af163eb0553d0e9adca2a99
VicFrank <matthewshattil@gmail.com>
2020-03-31 22:56:19 -0500

    fixed mystats page being too low

--- client/src/components/pages/MyStats.vue
+++ client/src/components/pages/MyStats.vue
@@ -2,32 +2,31 @@
   <div class="main-layout__content">
     <div class="content">
       <h1 class="page-title">My Stats</h1>
-    </div>
-
-    <div class="container">
-      <div class="row">
-        <div class="col-xl-12">
-          <div class="my-stats">
-            <table class="table">
-              <tbody>
-                <tr>
-                  <td class="tb-head">mmr</td>
-                  <td class="tb-head">poggers</td>
-                  <td class="tb-head">wins</td>
-                  <td class="tb-head">losses</td>
-                  <td class="tb-head">abandoned</td>
-                  <td class="tb-head">time played</td>
-                </tr>
-                <tr>
-                  <td>{{ playerStats.mmr }}</td>
-                  <td>{{ playerStats.poggers }}</td>
-                  <td>{{ playerStats.wins }}</td>
-                  <td>{{ playerStats.losses }}</td>
-                  <td>{{ playerStats.abandoned }}</td>
-                  <td>{{ playerStats.time_played | hhmmss }}</td>
-                </tr>
-              </tbody>
-            </table>
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-12">
+            <div class="my-stats">
+              <table class="table">
+                <tbody>
+                  <tr>
+                    <td class="tb-head">mmr</td>
+                    <td class="tb-head">poggers</td>
+                    <td class="tb-head">wins</td>
+                    <td class="tb-head">losses</td>
+                    <td class="tb-head">abandoned</td>
+                    <td class="tb-head">time played</td>
+                  </tr>
+                  <tr>
+                    <td>{{ playerStats.mmr }}</td>
+                    <td>{{ playerStats.poggers }}</td>
+                    <td>{{ playerStats.wins }}</td>
+                    <td>{{ playerStats.losses }}</td>
+                    <td>{{ playerStats.abandoned }}</td>
+                    <td>{{ playerStats.time_played | hhmmss }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
@@ -39,7 +38,7 @@
 export default {
   data: () => ({
     error: "",
-    playerStats: {},
+    playerStats: {}
   }),
 
   created() {
@@ -48,7 +47,7 @@ export default {
       .then(playerStats => {
         this.playerStats = playerStats;
       });
-  },
+  }
 };
 </script>
 
