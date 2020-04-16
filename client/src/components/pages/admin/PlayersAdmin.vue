e1f2624e292d6add485a44be40de36cb148a3f71
VicFrank <matthewshattil@gmail.com>
2020-04-16 01:50:58 -0500

    added steamID to admin page

--- client/src/components/pages/admin/PlayersAdmin.vue
+++ client/src/components/pages/admin/PlayersAdmin.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <b-alert v-if="error != ''" show variant="danger" dismissible>{{error}}</b-alert>
-    <b-alert v-if="success != ''" show variant="success" dismissible>Transaction Success!</b-alert>
+    <b-alert v-if="error != ''" show variant="danger" dismissible>{{
+      error
+    }}</b-alert>
+    <b-alert v-if="success != ''" show variant="success" dismissible
+      >Transaction Success!</b-alert
+    >
     <div v-if="loading" class="d-flex justify-content-center mb-3">
       <b-spinner label="Loading..."></b-spinner>
     </div>
-    <b-form-input v-model="steamID" placeholder="Enter Steam ID..."></b-form-input>
+    <div>Your SteamID is: {{ yourSteamID }}</div>
+    <b-form-input
+      v-model="steamID"
+      placeholder="Enter Steam ID..."
+    ></b-form-input>
     <b-button
       type="submit"
       variant="primary"
       class="mt-3"
       @click="getPlayerData(steamID)"
-    >Find Player</b-button>
+      >Find Player</b-button
+    >
 
     <b-card
       v-if="playerData.username"
@@ -21,16 +30,25 @@
     >
       <b-list-group flush>
         <b-list-group-item>
-          Poggers {{playerData.poggers}}
-          <b-button v-b-modal.modal-1 variant="success" size="sm" class="ml-5">+</b-button>
+          Poggers {{ playerData.poggers }}
+          <b-button v-b-modal.modal-1 variant="success" size="sm" class="ml-5"
+            >+</b-button
+          >
           <b-modal id="modal-1" title="Give Poggers" @ok="givePoggers">
             <b-form-input type="number" v-model="poggers"></b-form-input>
           </b-modal>
         </b-list-group-item>
-        <b-list-group-item>Patreon Level {{playerData.patreon_level}}</b-list-group-item>
-        <b-list-group-item>MMR {{playerData.mmr}}</b-list-group-item>
-        <b-list-group-item>Battle Pass XP: {{playerData.battlePass.total_experience}}</b-list-group-item>
-        <b-list-group-item>Battle Pass Tier: {{playerData.battlePass.tier}}</b-list-group-item>
+        <b-list-group-item
+          >Patreon Level {{ playerData.patreon_level }}</b-list-group-item
+        >
+        <b-list-group-item>MMR {{ playerData.mmr }}</b-list-group-item>
+        <b-list-group-item
+          >Battle Pass XP:
+          {{ playerData.battlePass.total_experience }}</b-list-group-item
+        >
+        <b-list-group-item
+          >Battle Pass Tier: {{ playerData.battlePass.tier }}</b-list-group-item
+        >
       </b-list-group>
     </b-card>
 
@@ -38,11 +56,17 @@
       <b-button v-b-toggle.collapse-1 variant="primary">Give Items</b-button>
       <b-collapse id="collapse-1" class="mt-2 mb-2">
         <b-card>
-          <b-form-select v-model="selected" :options="options" multiple></b-form-select>
+          <b-form-select
+            v-model="selected"
+            :options="options"
+            multiple
+          ></b-form-select>
           <div class="mt-3">
             <strong>{{ selected }}</strong>
           </div>
-          <b-button variant="success" class="mt-2" @click="addCosmeticItem">Add</b-button>
+          <b-button variant="success" class="mt-2" @click="addCosmeticItem"
+            >Add</b-button
+          >
         </b-card>
       </b-collapse>
       <b-list-group-item
@@ -50,7 +74,7 @@
         :key="cosmetic.cosmetic_id + cosmetic.created + cosmetic.equipped"
         class="mt-2"
       >
-        {{cosmetic.cosmetic_id}}
+        {{ cosmetic.cosmetic_id }}
         <span v-if="cosmetic.equipped">: Equipped</span>
       </b-list-group-item>
     </b-card>
@@ -68,43 +92,49 @@ export default {
     selected: [],
     loading: false,
     success: "",
-    poggers: 0
+    poggers: 0,
   }),
 
   created() {
     fetch(`/api/cosmetics`)
-      .then(res => res.json())
-      .then(cosmetics => {
-        this.options = cosmetics.map(cosmetic => ({
+      .then((res) => res.json())
+      .then((cosmetics) => {
+        this.options = cosmetics.map((cosmetic) => ({
           value: cosmetic,
-          text: cosmetic.cosmetic_id
+          text: cosmetic.cosmetic_id,
         }));
         this.options.push({
           value: null,
-          text: "Select a cosmetic to add..."
+          text: "Select a cosmetic to add...",
         });
       })
-      .catch(err => (this.error = err));
+      .catch((err) => (this.error = err));
+  },
+
+  computed: {
+    yourSteamID() {
+      return this.$store.state.auth.userSteamID;
+    },
   },
 
   methods: {
     getPlayerData(steamID) {
       fetch(`/api/players/${steamID}`)
-        .then(res => res.json())
-        .then(playerData => {
+        .then((res) => res.json())
+        .then((playerData) => {
           this.playerData = playerData;
           if (!this.playerData.username) {
             this.error = "No player with this SteamID found";
           }
         })
-        .catch(err => (this.error = err));
+        .catch((err) => (this.error = err));
 
       fetch(`/api/players/${steamID}/cosmetics`)
-        .then(res => res.json())
-        .then(cosmetics => {
+        .then((res) => res.json())
+        .then((cosmetics) => {
           this.cosmetics = cosmetics;
         })
-        .catch(err => (this.error = err));
+        .catch((err) => (this.error = err));
     },
     addCosmeticItem() {
       const selectedItems = this.selected;
@@ -116,7 +146,7 @@ export default {
             name: item.cosmetic_id,
             effect: "-1",
             level: "1",
-            amount: "1"
+            amount: "1",
           });
         } else {
           items[item.cosmetic_id] = "1";
@@ -125,17 +155,18 @@ export default {
       const transaction = {
         itemTransaction: {
           items,
-          companions
-        }
+          companions,
+        },
       };
 
+      this.selected = [];
       this.doTransaction(transaction);
     },
     givePoggers() {
       this.doTransaction({
         itemTransaction: {
-          poggers: this.poggers
-        }
+          poggers: this.poggers,
+        },
       });
     },
     doTransaction(transaction) {
@@ -144,22 +175,25 @@ export default {
       fetch(`/api/players/${this.steamID}/transaction`, {
         method: "post",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(transaction)
+        body: JSON.stringify(transaction),
       })
-        .then(res => res.json())
-        .then(res => {
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
           this.loading = false;
           this.success = res.message;
           this.getPlayerData(this.steamID);
+          document.documentElement.scrollTop = 0;
         })
-        .catch(err => {
+        .catch((err) => {
           this.error == err;
           this.loading = false;
+          document.documentElement.scrollTop = 0;
         });
-    }
-  }
+    },
+  },
 };
 </script>
 
