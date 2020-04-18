c4e46f3e7fe76a0fb81f8e3ba0c1dec83914dd53
VicFrank <matthewshattil@gmail.com>
2020-04-18 18:05:23 -0500

    added faq

---
+++ client/src/components/pages/learn/FAQ.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="main-layout__content">
+    <div class="container">
+      <h1 class="page-title">Guardians</h1>
+      <div class="question">Is this Free to Play?</div>
+      <div
+        class="answer"
+      >Yes! Dota 2 is free to play and this custom game within the client is also free.</div>
+      <div class="question">How do I install?</div>
+      <div
+        class="answer"
+      >Open your Dota 2, navigate to the Arcade tab, and search for Path of Guardians. Install the game and you're good to go!</div>
+      <div
+        class="question"
+      >I'm getting an error when loading a game "Custom game archive does not match the lobby leader"</div>
+      <div class="answer">
+        This is a very common issue with custom games. The error message essentially means that you or the host's custom game are not fully updated. There are a few ways to ensure you have the latest update.
+        <ul>
+          <li>
+            Option 1:
+            <ul>
+              <li>Uninstall the custom game</li>
+              <li>Close your Dota client completely</li>
+              <li>Open your Dota again</li>
+              <li>Install the Custom game mode</li>
+            </ul>
+          </li>
+          <li>
+            Option 2:
+            <ul>
+              <li>Uninstall the Custom game</li>
+              <li>Install it again</li>
+              <li>If you see a download progress bar, then it should work. If you do NOT see it, it definitely did NOT work.</li>
+            </ul>
+          </li>
+          <li>
+            Option 3:
+            <ul>
+              <li>Close your Dota</li>
+              <li>Go into Steam/Library and then click on the Downloads button at the bottom</li>
+              <li>There should be a download button for Dota 2 Workshop</li>
+              <li>Once completed, Open Dota</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div class="question">What are Guardians?</div>
+      <div
+        class="answer"
+      >Guardians are Mini-bosses that can be found on opposite sides of the map. Whichever team slays the beast will be provided with a buff for 5 minutes. You can learn more about their skills and buffs here. There are currently 3 Guardians in the game (only 2 can be spawned at one time).</div>
+      <div class="question">What are POGGERS?</div>
+      <div
+        class="answer"
+      >POGGERS stands for Path of Guardians Growth Experience Reward System. Yes, we forced that one pretty hard. POGGERS are the in-game currency for this custom game. You this currency simply by playing the game. You can also purchase them with real money. Visit our Store to see a full list of cosmetics that can be purchased with POGGERS.</div>
+      <div class="question">Is the Battle Pass free?</div>
+      <div
+        class="answer"
+      >Yes! You will receive XP for simply playing games, which will level your Battlepass. You can also receive XP by completing achievements and daily quests. In addition, you can purchase an XP booster which will help you level your Battlepass faster.</div>
+      <div class="question">How do I unlock the 4th and 5th pick slots?</div>
+      <div
+        class="answer"
+      >The 4th slot can be unlocked by gaining level 40 in your Battlepass. The 5th slot can be unlocked by spending POGGERS.</div>
+      <div class="question">How do re-rolls work and how can I get more?</div>
+      <div
+        class="answer"
+      >Every game you will receive 1 free re-roll. If you want to re-roll more, you must use consumable re-roll tokens. These tokens can be collected by either leveling up your Battlepass or by purchasing in the store.</div>
+      <div class="question">Where can I hang out with other POG players?</div>
+      <div class="answer">
+        Come chill in our
+        <a href="http://discord.gg/sunsfan" target="_blank">Discord Channel</a>
+      </div>
+      <div class="question">Will there be custom heroes?</div>
+      <div
+        class="answer"
+      >Yes! We have many custom heroes in the works that we plan to announce more on in the near future.</div>
+      <div class="question">Where can I report bugs?</div>
+      <div class="answer">Please report bugs via the DotaCinema subreddit.</div>
+      <div
+        class="question"
+      >When there is a gameplay patch for Dota, does it automatically apply to POG?</div>
+      <div class="answer">idk lol sunsfan didn't include this answer</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.question {
+  color: #0b86c4;
+  font-size: 2rem;
+  margin-bottom: 0.5rem;
+  font-weight: 500;
+  line-height: 1.2;
+}
+
+.answer {
+  margin-bottom: 0.5rem;
+  font-size: 1.2rem;
+  line-height: 1.2;
+}
+</style>
