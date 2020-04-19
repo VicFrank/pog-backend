69634ecf4ba57901cbcb7d7c55846d1d468e37fc
sscotten <sscotten@gmail.com>
2020-04-18 19:31:33 -0700

    Update FAQ.vue

--- client/src/components/pages/learn/FAQ.vue
+++ client/src/components/pages/learn/FAQ.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="main-layout__content">
     <div class="container">
-      <h1 class="page-title">Guardians</h1>
+      <h1 class="page-title">Frequently Asked Questions</h1>
       <div class="question">Is this Free to Play?</div>
       <div
         class="answer"
-      >Yes! Dota 2 is free to play and this custom game within the client is also free.</div>
+      >Yes! Dota 2 is completely free to play and this custom game within the client is also free.</div>
       <div class="question">How do I install?</div>
       <div
         class="answer"
@@ -22,7 +22,7 @@
               <li>Uninstall the custom game</li>
               <li>Close your Dota client completely</li>
               <li>Open your Dota again</li>
-              <li>Install the Custom game mode</li>
+              <li>Install the Custom game mode (You should see a download progress bar. If it is instant, then it did not work)</li>
             </ul>
           </li>
           <li>
@@ -47,23 +47,23 @@
       <div class="question">What are Guardians?</div>
       <div
         class="answer"
-      >Guardians are Mini-bosses that can be found on opposite sides of the map. Whichever team slays the beast will be provided with a buff for 5 minutes. You can learn more about their skills and buffs here. There are currently 3 Guardians in the game (only 2 can be spawned at one time).</div>
+      >Guardians are Mini-bosses that can be found on opposite sides of the map. Whichever team slays the beast will be provided with a buff for 5 minutes. You can learn more about their skills and buffs  <a href="https://www.pathofguardians.com/demo/guardians" target="_blank">here</a>. There are currently 3 Guardians in the game (only 2 can be spawned at one time).</div>
       <div class="question">What are POGGERS?</div>
       <div
         class="answer"
-      >POGGERS stands for Path of Guardians Growth Experience Reward System. Yes, we forced that one pretty hard. POGGERS are the in-game currency for this custom game. You this currency simply by playing the game. You can also purchase them with real money. Visit our Store to see a full list of cosmetics that can be purchased with POGGERS.</div>
+      >POGGERS stands for Path of Guardians Growth Experience Reward System. Yes, we forced that one pretty hard. POGGERS are the in-game currency for this custom game. You get this currency simply by playing the game. You can also purchase them with real money. <a href="https://www.pathofguardians.com/demo/store" target="_blank">Visit our Store</a> to see a full list of cosmetics that can be purchased with POGGERS.</div>
       <div class="question">Is the Battle Pass free?</div>
       <div
         class="answer"
-      >Yes! You will receive XP for simply playing games, which will level your Battlepass. You can also receive XP by completing achievements and daily quests. In addition, you can purchase an XP booster which will help you level your Battlepass faster.</div>
-      <div class="question">How do I unlock the 4th and 5th pick slots?</div>
+      >Yes! You will receive XP for simply playing games, which will level your Battle Pass. You can also receive XP by completing achievements and daily quests. In addition, you can purchase an XP booster which will help you level your Battle Pass even faster.</div>
+      <div class="question">How do I unlock the 4th picking slot?</div>
       <div
         class="answer"
-      >The 4th slot can be unlocked by gaining level 40 in your Battlepass. The 5th slot can be unlocked by spending POGGERS.</div>
+      >The 4th slot can be unlocked by gaining level 40 in your Battle Pass.</div>
       <div class="question">How do re-rolls work and how can I get more?</div>
       <div
         class="answer"
-      >Every game you will receive 1 free re-roll. If you want to re-roll more, you must use consumable re-roll tokens. These tokens can be collected by either leveling up your Battlepass or by purchasing in the store.</div>
+      >Every game you will receive 1 free re-roll. The Battle Pass has several unlocks that will give you additional re-rolls per day.</div>
       <div class="question">Where can I hang out with other POG players?</div>
       <div class="answer">
         Come chill in our
@@ -74,11 +74,11 @@
         class="answer"
       >Yes! We have many custom heroes in the works that we plan to announce more on in the near future.</div>
       <div class="question">Where can I report bugs?</div>
-      <div class="answer">Please report bugs via the DotaCinema subreddit.</div>
+      <div class="answer">Please report bugs via the <a href="https://www.reddit.com/r/DotaCinema/" target="_blank">DotaCinema subreddit</a>.</div>
       <div
         class="question"
       >When there is a gameplay patch for Dota, does it automatically apply to POG?</div>
-      <div class="answer">idk lol sunsfan didn't include this answer</div>
+      <div class="answer">This is a tricky question to answer. For number changes, it almost always updates automatically. There are some exceptions for certain items that we've had to re-create with special effects (Pipe, Shivas, etc). When things like hero stats/talents/aghs are changed, they should work right away in our mod. However, there are times when an update could break custom games in general. In these rare cases, you can expect a speedy update from us to remedy the situation.</div>
     </div>
   </div>
 </template>
