fe2595730388d77e3fcd30d0e441396c16174db2
VicFrank <matthewshattil@gmail.com>
2020-04-29 11:26:12 -0500

    fixed battle pass images not being clickable

--- client/src/components/pages/cosmetics/BattlePass.vue
+++ client/src/components/pages/cosmetics/BattlePass.vue
@@ -16,13 +16,13 @@
                 <img src="./images/bp_placeholder.png" alt="placeholder" />
               </template>
               <template v-else>
-                <b-img-lazy
+                <img
                   v-if="getItemImage(i)"
                   :src="getItemImage(i)"
                   :alt="getRewardItem(i)"
                   @click="$bvModal.show(`bp-modal-${i}`)"
                 />
-                <b-img-lazy
+                <img
                   v-for="j in getChestAmount(i)"
                   :key="i + ' ' + j"
                   v-bind:src="getChestImage(i)"
--- client/src/components/pages/learn/Media.vue
+++ client/src/components/pages/learn/Media.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="main-layout__content">
     <div class="content">
-      <h1 class="page-title">Media</h1>
+      <h1 class="page-title">Videos</h1>
       <div class="container">
-        <b-embed
-          type="iframe"
-          aspect="16by9"
-          src="https://www.youtube.com/embed/FGWtWZAcY70?list=PLpZEgzu1Qm_sDFx8IVRFjcjaBQFuUgWnh"
-          allowfullscreen
-        ></b-embed>
+        <h3>POG Overview</h3>
+        <div class="mb-4">
+          <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/twkWKkZzBEM"
+            allowfullscreen
+          ></b-embed>
+        </div>
+        <h3>Picking your Hero</h3>
+        <div class="mb-4">
+          <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/XkFGurRfD0A"
+            allowfullscreen
+          ></b-embed>
+        </div>
+        <h3>Battle Pass</h3>
+        <div class="mb-4">
+          <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/2b6lAcxA2AM"
+            allowfullscreen
+          ></b-embed>
+        </div>
       </div>
     </div>
   </div>
