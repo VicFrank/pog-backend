86dd5202303b42c083bfa20cd7e9d79cb0d89047
VicFrank <matthewshattil@gmail.com>
2020-05-27 23:36:20 -0500

    database improvements

--- testing/initialize-db.js
+++ testing/initialize-db.js
@@ -249,7 +249,7 @@ async function initializeAdmins() {
   await loadBattlePass();
   await loadItemPrices();
   // await initPlayers();
-  // await addSampleGames(100);
+  await addSampleGames(10);
   await initializeAdmins();
   await addRealSample();
 })();
