82beb38bd509d53454a8b67da1f764c5c7a9de14
VicFrank <matthewshattil@gmail.com>
2020-10-25 00:20:12 -0500

    updated changelog parsing

--- client/src/components/pages/Changelog.vue
+++ client/src/components/pages/Changelog.vue
@@ -7,8 +7,11 @@
           <div class="col-xl-12">
             <div class="pl-3 change-log">
               <div v-for="change of changes" :key="change.timestamp">
-                <h3 v-if="change.title">{{change.title}}</h3>
-                <h4>{{$t('changelog.update')}}: {{ getTime(change.timestamp * 1000) }}</h4>
+                <h3 v-if="change.title">{{ change.title }}</h3>
+                <h4>
+                  {{ $t("changelog.update") }}:
+                  {{ getTime(change.timestamp * 1000) }}
+                </h4>
                 <ul>
                   <li v-for="line of change.lines" :key="line">{{ line }}</li>
                 </ul>
@@ -25,17 +28,23 @@
 export default {
   data: () => ({
     error: "",
-    changes: []
+    changes: [],
   }),
 
   created() {
     fetch("/api/steam/changelog")
-      .then(res => res.json())
-      .then(res => {
+      .then((res) => res.json())
+      .then((res) => {
         let changes = res.changes;
 
-        changes = changes.map(change => {
-          const description = change.change_description;
+        changes = changes.map((change) => {
+          let description = change.change_description;
+          const tags = ["[b]", "[/b]", "[*]", "[/list]"];
+
+          for (let tag of tags) {
+            description = description.replace(tag, "");
+          }
+
           // parse underlines
           let underlineStart = description.indexOf("[u]");
           let underlineEnd = description.indexOf("[/u]");
@@ -50,31 +59,35 @@ export default {
           if (title) lines.shift();
 
           lines = lines
-            .filter(line => line != "")
-            .map(line => {
+            .map((line) => {
+              //filter out all tags
+              for (let tag of tags) {
+                line = line.replace(tag, "");
+              }
               if (line.charAt(0) === "*") {
                 return line.substring(2);
               }
               return line;
-            });
+            })
+            .filter((line) => line != "");
 
           return {
             ...change,
             title,
-            lines
+            lines,
           };
         });
 
         this.changes = changes;
       })
-      .catch(err => console.error(err));
+      .catch((err) => console.error(err));
   },
 
   methods: {
     getTime(timestamp) {
       return new Date(timestamp).toLocaleDateString("en-us");
-    }
-  }
+    },
+  },
 };
 </script>
 
