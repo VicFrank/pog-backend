8950a52f6682b331e3ab8fd62e5651792b782308
VicFrank <matthewshattil@gmail.com>
2022-02-11 17:45:43 -0600

    updated horse image

--- client/src/components/pages/temp/Horse.vue
+++ client/src/components/pages/temp/Horse.vue
@@ -1,39 +1,5 @@
 <template>
-  <div class="main-layout__content">
-    <div class="container">
-      <div class="row">
-        <div class="col-xl-12">
-          <h1 class="page-title">Horse</h1>
-          <b-img
-            :src="require('./images/Arcane.png')"
-            fluid
-            alt="Arcane"
-          ></b-img>
-          <b-img :src="require('./images/Blood.png')" fluid alt="Blood"></b-img>
-          <b-img :src="require('./images/Earth.png')" fluid alt="Earth"></b-img>
-          <b-img
-            :src="require('./images/Ethereal.png')"
-            fluid
-            alt="Ethereal"
-          ></b-img>
-          <b-img :src="require('./images/Fire.png')" fluid alt="Fire"></b-img>
-          <b-img :src="require('./images/Ice.png')" fluid alt="Ice"></b-img>
-          <b-img
-            :src="require('./images/Nature.png')"
-            fluid
-            alt="Nature"
-          ></b-img>
-          <b-img
-            :src="require('./images/Poison.png')"
-            fluid
-            alt="Poison"
-          ></b-img>
-          <b-img :src="require('./images/Storm.png')" fluid alt="Storm"></b-img>
-          <b-img :src="require('./images/Void.png')" fluid alt="Void"></b-img>
-          <b-img :src="require('./images/Water.png')" fluid alt="Water"></b-img>
-          <b-img :src="require('./images/Wind.png')" fluid alt="Wind"></b-img>
-        </div>
-      </div>
-    </div>
+  <div class="main-layout__content" style="overflow: auto">
+    <b-img :src="require('./images/elements.png')" alt="Elements"></b-img>
   </div>
 </template>
