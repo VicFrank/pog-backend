ed61b70eee18c3ae6295ca7baddf1bdfce5026e0
VicFrank <matthewshattil@gmail.com>
2022-02-14 18:55:53 -0600

    margin

--- client/src/components/pages/temp/Horse.vue
+++ client/src/components/pages/temp/Horse.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="main-layout__content" style="overflow: auto">
-    <b-img :src="require('./images/elements.png')" alt="Elements"></b-img>
+    <b-img
+      style="margin-left: 200px"
+      :src="require('./images/elements.png')"
+      alt="Elements"
+    ></b-img>
   </div>
 </template>
